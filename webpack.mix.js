4c7ef2d9cc3708f6a3539fa14e21e59eed4b1791
Lucas Vigneron <lucas.vigneron@adeliom.com>
2022-02-11 13:28:20 +0100

    Ajout fichier style

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
 
-mix.js('src/js/AdeliomMap.js', 'dist')
+mix.js('src/js/AdeliomMap.js', 'dist/js')
+    .sass('src/scss/AdeliomMap.scss', 'css')
     .setPublicPath('dist');
