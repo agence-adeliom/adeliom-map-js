c0aa3a798969bc5608b3182b046783be1c00b543
Lucas Vigneron <lucas.vigneron@adeliom.com>
2022-03-09 14:48:30 +0100

    Correctifs dépendances TypeScript

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ let mix = require('laravel-mix');
 
 mix
     .ts('src/js/AdeliomMap.ts', 'dist/js')
-    .ts('src/js/demo.js', 'dist/js')
+    .js('src/js/demo.js', 'dist/js')
     .sass('src/scss/AdeliomMap.scss', 'css')
     .sass('src/scss/demo.scss', 'css')
     .copyDirectory('src/img', 'dist/img')
